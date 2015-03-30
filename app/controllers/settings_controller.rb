b4b4ee07cee12b0b77a590f60b696e9ca5695897
Ross Nelson <ross.nelson@gmail.com>
2015-03-29 20:49:55 -0500

    Disable password changes for now

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -3,25 +3,28 @@ class SettingsController < ApplicationController
   end
 
   def password
-    user = User.find_by_id(session[:user_id])
-    @pass_form = ChangePasswordForm.new(user)
+    redirect_to settings_path, notice: 'Sorry, password changes not supported'
 
-    if @pass_form.submit(params)
-      redirect_to settings_path, notice: 'Password changed'
-    else
-      errors  = "<div id='error_explanation'>"
-      errors += "<h2>#{@pass_form.errors.count} error(s) prohibited this project from being saved:</h2>"
-      errors == "<ul>"
-      @pass_form.errors.each do |message|
-        errors += "<li>#{message}</li>"
-      end
-      errors += "</ul>"
-      errors += "</div>"
+    # user = User.find_by_id(session[:user_id])
+    # @pass_form = ChangePasswordForm.new(user)
+    #
+    # if @pass_form.submit(params)
+    #   redirect_to settings_path, notice: 'Password changed'
+    # else
+    #   errors  = "<div id='error_explanation'>"
+    #   errors += "<h2>#{@pass_form.errors.count} error(s) prohibited this project from being saved:</h2>"
+    #   errors == "<ul>"
+    #   @pass_form.errors.each do |message|
+    #     errors += "<li>#{message}</li>"
+    #   end
+    #   errors += "</ul>"
+    #   errors += "</div>"
+    #
+    #   redirect_to settings_path, notice: errors.html_safe
 
-      redirect_to settings_path, notice: errors.html_safe
       # @errors = @pass_form.errors
       #redirect_to settings_path, errors: @errors
       # redirect_to settings_path, notice: @errors
-    end
+    # end
   end
 end
